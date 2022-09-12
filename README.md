448377dafed189f5d92c302d90ad3c970c2dfe25
boom <nuntipatcpe@gmail.com>
2022-09-12 19:47:52 +0700

    feat : upload file

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
+## Declaring
+
 [Declaring Models](https://gorm.io/docs/models.html#content-inner)
 
-jwt
+---
 
-```
-go get github.com/dgrijalva/jwt-go
-```
+## Jwt-go (JSON Web Tokens)
+
+- jwt [ref](https://github.com/golang-jwt/jwt)
+
+  ```
+  go get github.com/dgrijalva/jwt-go
+  ```
+
+---
--- api/product.api.go
+++ api/product.api.go
@@ -1,7 +1,11 @@
 package api
 
 import (
+	"fmt"
+	"os"
+	model "stock/Model"
 	"stock/interceptor"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -18,5 +22,20 @@ func getProduct(ctx *gin.Context) {
 	ctx.JSON(200, gin.H{"result": "Get product", "username": ctx.GetString("jwt_username")})
 }
 func createProduct(ctx *gin.Context) {
-	ctx.JSON(200, gin.H{"result": "Create Product"})
+	// ctx.JSON(200, gin.H{"result": "Create Product"})
+
+	product := model.Product{}
+	product.Name = ctx.PostForm("name")
+	product.Stock, _ = strconv.ParseInt(ctx.PostForm("stock"), 10, 64) //base 10 64bit
+	product.Price, _ = strconv.ParseFloat(ctx.PostForm("price"), 64)   //64bit
+	image, _ := ctx.FormFile("image")
+	product.Image = image.Filename
+
+	// extension := filepath.Ext(image.Filename)
+	// fileName := fmt.Sprintf("%d%s", product.ID, extension)
+	runningDir, _ := os.Getwd()
+	filepath := fmt.Sprintf("%s/uploaded/images/%s", runningDir, image.Filename)
+	ctx.SaveUploadedFile(image, filepath)
+
+	ctx.JSON(200, gin.H{"result": product})
 }
