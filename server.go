a76cb3b0d469f162d96f0a5285eecc4f99d8fb84
boom <nuntipatcpe@gmail.com>
2022-09-14 14:45:47 +0700

    Deploy

--- server.go
+++ server.go
@@ -1,19 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"main/api"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	//set * Running in "debug" mode. Switch to "release" mode in production
-	gin.SetMode(gin.ReleaseMode)
-	router := gin.New()
+	// gin.SetMode(gin.ReleaseMode)
+	// router := gin.New()
 
-	// router := gin.Default()
+	router := gin.Default()
 	router.Static("/images", "./uploaded/images")
-
 	api.Setup(router)
-	router.Run(":8081")
+
+	var port = os.Getenv("PORT")
+	if port == "" {
+		fmt.Print("No Port In Heroku")
+		router.Run()
+
+	} else {
+		fmt.Print("Environment Port : " + port)
+		router.Run(":" + port)
+	}
+	// router.Run(":8081")
 }
